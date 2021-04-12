1edddb72cbba2adba846262132e1b6b83c7c337d
aniket1418 <17btrcs029@jainuniversity.ac.in>
2021-04-12 11:38:14 +0530

    cv2 window creation for the screen

---
+++ AirCanva.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+from collections import deque
+
+# default called trackbar function
+
+
+def setValues(x):
+    print("")
+
+
+# Creating the trackbars needed for adjusting the marker colour
+cv2.namedWindow("Color detectors")
+cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180, setValues)
+cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255, setValues)
+cv2.createTrackbar("Upper Value", "Color detectors", 255, 255, setValues)
+cv2.createTrackbar("Lower Hue", "Color detectors", 64, 180, setValues)
+cv2.createTrackbar("Lower Saturation", "Color detectors", 72, 255, setValues)
+cv2.createTrackbar("Lower Value", "Color detectors", 49, 255, setValues)
